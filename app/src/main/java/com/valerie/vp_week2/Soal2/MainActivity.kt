07dde181e2e54a18807669055ff9427abb80a497
valeyy <marsha.valerie.wibowo@gmail.com>
2026-09-24 10:20:34 +0700

    removed 1 import

--- app/src/main/java/com/valerie/vp_week2/Soal2/MainActivity.kt
+++ app/src/main/java/com/valerie/vp_week2/Soal2/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.valerie.vp_week2.Soal2.myTravelView
 import com.valerie.vp_week2.ui.theme.Vp_week2Theme
 
 class MainActivity : ComponentActivity() {
